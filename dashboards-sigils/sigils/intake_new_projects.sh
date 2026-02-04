90cdc093d766a7d4cbb3d3fd13c875beadcea3cc
Laure Vancau <laure.vancauwenberghe@epfl.ch>
2026-02-04 10:39:28 +0100

    feat: mordred restart script for new projects

---
+++ dashboards-sigils/sigils/intake_new_projects.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+WATCHED_FILE="../default_grimoirelab-settings/projects.json"
+STATE_FILE="/tmp/projects_file_checksum_mordred"
+
+if [ ! -f "$WATCHED_FILE" ]; then
+    echo "The file you want to watch or is being watched does not exist: $WATCHED_FILE"
+    exit 1
+fi
+
+CURRENT_SUM=$(sha256sum "$WATCHED_FILE" | awk '{print $1}')
+
+# If state file does not exist, create it
+if [ ! -f "$STATE_FILE" ]; then
+    echo "$CURRENT_SUM" > "$STATE_FILE"
+    exit 0
+fi
+
+PREVIOUS_SUM=$(cat "$STATE_FILE")
+
+if [ "$CURRENT_SUM" != "$PREVIOUS_SUM" ]; then
+    echo "Projects file changed. Restarting docker compose for mordred..."
+
+    docker compose -p docker-compose-mordred-1 restart
+
+    echo "$CURRENT_SUM" > "$STATE_FILE"
+fi
